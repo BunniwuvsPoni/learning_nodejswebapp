e63a8d6436878f71e3e675e210f1379372abcd8a
Tony Wang <zichutonywang@outlook.com>
2019-06-07 10:52:58 -0400

    Created working hello_world.js.

--- hello_world/hello_world.js
+++ hello_world/hello_world.js
@@ -1,2 +1,11 @@
-﻿console.log("Hello world!");
+﻿console.log('Hello world!');
 
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+}).listen(port);
+
+console.log('Server has been initiated.');

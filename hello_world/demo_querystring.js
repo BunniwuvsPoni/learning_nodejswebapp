24c08bcf581eb503ee91d2dcea8e1e6d865aebbc
Tony Wang <zichutonywang@outlook.com>
2019-06-07 11:06:18 -0400

    demo_querystring.js reads varibles passed in the url and prints the specific variables back out (month and year).

---
+++ hello_world/demo_querystring.js
@@ -0,0 +1,11 @@
+﻿var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
+}).listen(1337);
+
+//http://localhost:1337/?year=2017&month=July will produce 2017 July

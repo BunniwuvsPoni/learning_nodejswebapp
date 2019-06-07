0788ce63bb45af127966f8d5f8ea325e4c8e22a2
Tony Wang <zichutonywang@outlook.com>
2019-06-07 10:41:39 -0400

    Add project files.

---
+++ hello_world/server.js
@@ -0,0 +1,12 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World - this is the initial default page\n');
+}).listen(port);
+
+
+
+console.log('Server running on port: ' + port);

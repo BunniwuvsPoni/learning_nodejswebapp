5dd2cc3e4f8d9e9d09e705d94df1b873b4f903e8
Tony Wang <zichutonywang@outlook.com>
2019-06-07 10:59:05 -0400

    Added date module to hello_world.js

--- hello_world/hello_world.js
+++ hello_world/hello_world.js
@@ -1,11 +1,14 @@
 ﻿console.log('Hello world!');
 
 var http = require('http');
+var dt = require('./myfirstmodule');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.write('Hello, world!');
+    res.write('The date and time are currently: ' + dt.myDateTime());
+    res.end();
 }).listen(port);
 
 console.log('Server has been initiated.');

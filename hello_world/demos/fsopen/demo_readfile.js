b34e5729ce352c3973eeb28b57064119ec0ba47f
Tony Wang <zichutonywang@outlook.com>
2019-06-08 19:52:25 -0400

    File is still now reading properly

--- hello_world/demos/fsopen/demo_readfile.js
+++ hello_world/demos/fsopen/demo_readfile.js
@@ -3,18 +3,9 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('./data.txt', function (err, data) {
-        //Not sure why callback triggers twice and the resulting data is null
-
-        console.log("Read the file");
-        console.log(data);
-        console.log("File read completed");
-
+    fs.readFile('data.txt', function (err, data) { // File is still now reading properly
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('Hello, world!');
-
-        //res.writeHead(200, { 'Content-Type': 'text/html' });
-        //res.write(data);
+        res.write(data);
         res.end();
     });
 }).listen(1337);

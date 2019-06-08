4d3286f5592ecef317a3149c061154c0d74e1c5d
Tony Wang <zichutonywang@outlook.com>
2019-06-08 19:41:53 -0400

    Still troubleshooting fs.readfile

--- hello_world/demos/fsopen/demo_readfile.js
+++ hello_world/demos/fsopen/demo_readfile.js
@@ -1,30 +1,20 @@
-﻿/*
+﻿
 var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('./fsopen/demofile1.html', function (err, data) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-}).listen(1337);
-*/
+    fs.readFile('demofile1.html', function (err, data) {
+        //Not sure why callback triggers twice and the resulting data is null
 
-var http = require('http');
+        console.log("Read the file");
+        console.log(data);
+        console.log("File read completed");
 
-http.createServer(function (req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('Hi');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('Hello, world!');
+
+        //res.writeHead(200, { 'Content-Type': 'text/html' });
+        //res.write(data);
         res.end();
+    });
 }).listen(1337);
-
-/*
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World - this is the initial default page\n');
-}).listen(port);
-*/
